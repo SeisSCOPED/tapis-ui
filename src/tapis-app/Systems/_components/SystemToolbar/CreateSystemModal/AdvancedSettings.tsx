414576bf5961a8029134acc5bfb639f6b556c381
Steven Oh <potato@wireless-10-147-62-180.public.utexas.edu>
2023-07-17 11:55:44 -0500

    reformatted the descriptions of the different options to be uniform

--- src/tapis-app/Systems/_components/SystemToolbar/CreateSystemModal/AdvancedSettings.tsx
+++ src/tapis-app/Systems/_components/SystemToolbar/CreateSystemModal/AdvancedSettings.tsx
@@ -30,14 +30,14 @@ const AdvancedSettings: React.FC<AdvancedSettingsProp> = ({ simplified }) => {
           name="rootDir"
           label="Root Directory"
           required={false}
-          description={`Root Directory`}
+          description={`Root directory`}
           aria-label="Input"
         />
         <FormikInput
           name="jobWorkingDir"
           label="Job Working Directory"
           required={false}
-          description={`Job Working Directory`}
+          description={`Job working directory`}
           aria-label="Input"
         />
         <FormikSelect
@@ -58,7 +58,7 @@ const AdvancedSettings: React.FC<AdvancedSettingsProp> = ({ simplified }) => {
           name="effectiveUserId"
           label="Effective User ID"
           required={false}
-          description={`Effective User ID`}
+          description={`Effective user id`}
           aria-label="Input"
         />
         <FormikCheck
@@ -77,7 +77,7 @@ const AdvancedSettings: React.FC<AdvancedSettingsProp> = ({ simplified }) => {
               data-testid="batchScheduler"
             >
               <option disabled value="">
-                Select an batch scheduler
+                Select a batch scheduler
               </option>
               {schedulerTypes.map((values) => {
                 return <option>{values}</option>;
@@ -87,14 +87,14 @@ const AdvancedSettings: React.FC<AdvancedSettingsProp> = ({ simplified }) => {
               name="batchSchedulerProfile"
               label="Batch Scheduler Profile"
               required={false}
-              description={`Batch Scheduler Profile`}
+              description={`Batch scheduler profile`}
               aria-label="Input"
             />
             <FormikInput
               name="batchDefaultLogicalQueue"
               label="Batch Default Logical Queue"
               required={false}
-              description={`Batch Default Logical Queue`}
+              description={`Batch default logical queue`}
               aria-label="Input"
             />
             <Collapse title="Batch Logical Queue Settings">
@@ -109,77 +109,77 @@ const AdvancedSettings: React.FC<AdvancedSettingsProp> = ({ simplified }) => {
                 name="hpcQueueName"
                 label="HPC Queue Name"
                 required={true}
-                description={`HPC Queue Name`}
+                description={`HPC queue name`}
                 aria-label="Input"
                 />
                 <FormikInput
                 name="maxJobs"
                 label="Max Jobs"
                 required={false}
-                description={`Maximum Jobs`}
+                description={`Maximum number of jobs`}
                 aria-label="Input"
                 />
                 <FormikInput
                 name="maxJobsPerUser"
                 label="Max Jobs Per User"
                 required={false}
-                description={`Maximum Jobs Per User`}
+                description={`Maximum number of jobs per user`}
                 aria-label="Input"
                 />
                 <FormikInput
                 name="minNodeCount"
                 label="Min Node Count"
                 required={false}
-                description={`Minimum Node Count`}
+                description={`Minimum number of nodes`}
                 aria-label="Input"
                 />
                 <FormikInput
                 name="maxNodeCount"
                 label="Max Node Count"
                 required={false}
-                description={`Maximum Node Count`}
+                description={`Maximum number of nodes`}
                 aria-label="Input"
                 />
                 <FormikInput
                 name="minCoresPerNode"
                 label="Min Cores Per Node"
                 required={false}
-                description={`Minimum Cores Per Node`}
+                description={`Minimum number of cores per node`}
                 aria-label="Input"
                 />
                 <FormikInput
                 name="maxCoresPerNode"
                 label="Max Cores Per Node"
                 required={false}
-                description={`Maximum Cores Per Node`}
+                description={`Maximum number of cores per node`}
                 aria-label="Input"
                 />
                 <FormikInput
                 name="minMemoryMB"
                 label="Min Memory MB"
                 required={false}
-                description={`Minimum Memory MB`}
+                description={`Minimum memory in MB`}
                 aria-label="Input"
                 />
                 <FormikInput
                 name="maxMemoryMB"
                 label="Max Memory MB"
                 required={false}
-                description={`Maximum Memory MB`}
+                description={`Maximum memory in MB`}
                 aria-label="Input"
                 />
                 <FormikInput
                 name="minMinutes"
                 label="Min Minutes"
                 required={false}
-                description={`Minimum Minutes`}
+                description={`Minimum number of minutes`}
                 aria-label="Input"
                 />
                 <FormikInput
                 name="maxMinutes"
                 label="Max Minutes"
                 required={false}
-                description={`Maximum Minutes`}
+                description={`Maximum number of minutes`}
                 aria-label="Input"
                 />
             </Collapse>
