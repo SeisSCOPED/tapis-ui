d147f80de6f1beca11341a965b7ccd07d17acbdb
Jackson Thetford <j.thetford@utexas.edu>
2023-10-05 14:46:03 -0500

    Added ability for users to see all public apps

---
+++ src/tapis-api/files/getContents.ts
@@ -0,0 +1,28 @@
+import { errorDecoder } from 'tapis-api/utils';
+import StreamSaver from 'streamsaver';
+
+const getContents = (
+  systemId: string,
+  path: string,
+  destination: string,
+  zip: boolean,
+  onStart: null | ((response: Response) => void),
+  basePath: string,
+  jwt: string
+): Promise<Response> => {
+  const url = `${basePath}/v3/files/content/${systemId}/${path}${
+    zip ? '?zip=true' : ''
+  }`;
+
+  const config = {
+    headers: {
+      'X-Tapis-Token': jwt,
+    },
+  };
+
+  return errorDecoder<Response>(() =>
+    fetch(url, config)
+  )
+};
+
+export default getContents;
