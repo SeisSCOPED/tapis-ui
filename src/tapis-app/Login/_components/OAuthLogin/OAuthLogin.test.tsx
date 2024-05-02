2b0c9893b6d9d0378e82484ecf457ca8391a75c8
Steven Oh <oh.jae.steven@utexas.edu>
2024-05-02 00:06:05 -0500

    fixed OAuthLogin test file

--- src/tapis-app/Login/_components/OAuthLogin/OAuthLogin.test.tsx
+++ src/tapis-app/Login/_components/OAuthLogin/OAuthLogin.test.tsx
@@ -1,11 +1,12 @@
-import { render, cleanup } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
+import renderComponent from 'utils/testing';
 import OAuthLogin from './OAuthLogin';
 
 afterEach(cleanup);
 
 describe('OAuthLogin', () => {
   it('render OAuthLogin component', () => {
-    const { getByTestId } = render(<OAuthLogin />);
+    const { getByTestId } = renderComponent(<OAuthLogin />);
     const tokenValue = getByTestId('token');
     expect(tokenValue).toBeInTheDocument();
   });
